568f00567da48d65a3fb24e54f4a64cc8c5e9d5d
dohaicuong <beatyshot@gmail.com>
2020-09-05 08:09:58 +1000

    node interface; login, signup mutation; test for node interface query

--- social-media/backend/prisma/seed.ts
+++ social-media/backend/prisma/seed.ts
@@ -5,5 +5,15 @@ const db = new PrismaClient()
 main()
 
 async function main() {
-  db.disconnect()
+  const users = [
+    { id: '1', email: 'beatyshot@gmail.com', password: 'vasasvsva', name: 'Yuki' },
+    { id: '2', email: 'beatyneko@gmail.com', password: 'vg3qevawq', name: 'Yami' },
+  ]
+
+  for (const user of users) {
+    await db.user.create({ data: user })
+  }
+  console.log('done')
+
+  db.$disconnect()
 }
--- social-media/backend/tests/__helpers.ts
+++ social-media/backend/tests/__helpers.ts
@@ -3,16 +3,12 @@ import { createTestContext as originalCreateTestContext, TestContext } from 'nex
 export function createTestContext() {
   let ctx = {} as TestContext
 
-  // @ts-ignore
   beforeAll(async () => {
     Object.assign(ctx, await originalCreateTestContext())
     await ctx.app.start()
   })
 
-  // @ts-ignore
   afterAll(async () => {
-    // TODO: wait for nexus fix @types
-    // @ts-ignore
     await ctx.app.db.client.$disconnect()
     await ctx.app.stop()
   })
